36cf6afb54b547af0ac50a74c8e8262d69b12c22
ilyakozhevn <kozhevilya@gmail.com>
2022-08-09 07:00:44 +0300

    Initial Commit

---
+++ CompanyInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompanyInfo
+//
+//  Created by Ilya Kozhevnikov on 09.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
